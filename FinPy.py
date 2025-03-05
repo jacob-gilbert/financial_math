b4056f926b40109918ebae649969a3772ddc71fd
jacob-gilbert <jacob.gilbert@richmond.edu>
2025-03-04 22:45:02 -0500

    added simple interest and discount

---
+++ FinPy.py
@@ -0,0 +1,26 @@
+# Simple Interest
+def simple_interest(pv, i, t):
+    return pv * (1 + i * t)
+
+def simple_interest_i(pv, fv, t):
+    return ((fv / pv) - 1) / t
+
+def simple_interest_t(pv, fv, i):
+    return ((fv / pv) - 1) / i
+
+def simple_interest_pv(fv, i, t):
+    return (fv / (1 + i * t))
+
+
+# Simple Discount
+def simple_discount(fv, d, t):
+    return fv * (1 - d * t)
+
+def simple_discount_d(fv, pv, t):
+    return (1 - (pv / fv)) / t
+
+def simple_discount_t(pv, fv, d):
+    return (1 - (pv / fv)) / d
+
+def simple_discount_fv(pv, d, t):
+    return (pv / (1 - d * t))
