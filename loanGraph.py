a8d3e0fb9e8f88334ef7f3ea820ffd9069f6965e
jacob-gilbert <jacob.gilbert@richmond.edu>
2025-06-25 20:27:35 -0400

    added second window functionality

--- loanGraph.py
+++ loanGraph.py
@@ -17,11 +17,14 @@ class MainWindow(QMainWindow):
         loan_label = QLabel()
         loan_label.setText("Input Loan Information Below:")
         loan_label.setSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Fixed)
-        self.grid.addWidget(loan_label, 5, 0)
+        self.grid.addWidget(loan_label, 0, 0)
 
         loan_button = QPushButton()
         loan_button.setText("Create Graph")
-        self.grid.addWidget(loan_button, 7, 0)
+        self.grid.addWidget(loan_button, 2, 0)
+
+        # connect button to function
+        loan_button.clicked.connect(lambda: self.on_loan_button_clicked(loan_prin.text(), loan_int.text(), loan_time.text()))
 
         loan_prin = QLineEdit()
         loan_prin.setPlaceholderText("Principal")
@@ -46,31 +49,37 @@ class MainWindow(QMainWindow):
         loan_info_layout.addWidget(loan_time)
 
         loan_container.setLayout(loan_info_layout)
-        self.grid.addWidget(loan_container, 6, 0)
-
-        # create the graph
-        self.graph = PlotWidget()
-        self.grid.addWidget(self.graph, 0, 0, 5, 3)
+        self.grid.addWidget(loan_container, 1, 0)
 
         self.grid.setSpacing(5)
         self.grid.setVerticalSpacing(0)
         self.setCentralWidget(container)
 
 
+    def on_loan_button_clicked(self, amount, interest, time):
+        self.plot_window = PlotWidget(amount, interest, time)
+        self.plot_window.show()
 
-class PlotWidget(QWidget):
-    def __init__(self):
+
+class PlotWidget(QMainWindow):
+    def __init__(self, amount, interest, time):
         super().__init__()
 
         # Layout
+        container = QWidget()
         grid = QGridLayout()
+
+        """temp_label = QLabel()
+        temp_label.setText(f"Amount: {amount}, Interest: {interest}, Time {time}")
+        grid.addWidget(temp_label)"""
 
         # create a canvas and add it to the layout
         self.canvas = FigureCanvas(Figure(figsize=(5,3)))
         grid.addWidget(self.canvas)
 
         # set our layout to the plot widget
-        self.setLayout(grid)
+        container.setLayout(grid)
+        self.setCentralWidget(container)
 
         self.plot()
 
@@ -78,8 +87,8 @@ class PlotWidget(QWidget):
         ax = self.canvas.figure.add_subplot(111)
         ax.plot([0, 1, 2, 3], [10, 1, 20, 3], marker="o")
         ax.set_title("Sample Plot")
-        ax.set_xlabel("X Axis")
-        ax.set_ylabel("Y Axis")
+        ax.set_xlabel("Years")
+        ax.set_ylabel("Total Amount")
 
 
 if __name__ == "__main__":
